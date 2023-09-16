fdb479ab74262c9a5491fcfb90b919b58598dc20
AdirDahari <AdirDahari@users.noreply.github.com>
2023-09-16 04:24:36 +0300

    finish math game

--- js/mathGame.js
+++ js/mathGame.js
@@ -1,8 +1,11 @@
-let currentLevel = ""; // difficulty
+let currentLevel = "";
 let score = 0;
 let op = ""; // Mathematical operations
 let num1 = 0;
 let num2 = 0;
+let sec = 59;
+let min = 1;
+let isGameOver = false;
 
 // switch between start container and game container. set the init by selected level
 const setLevel = (str) => {
@@ -12,26 +15,26 @@ const setLevel = (str) => {
         containerStart.style.display = "none";
         containerGame.style.display = "block";
         if (str === "Easy") {
-            initGame(10);
+            init(10);
         } else if (str === "Medium") {
-            initGame(100);
+            init(100);
         } else if (str === "Hard") {
-            initGame(1000);
+            init(1000);
         }
     }
 }
 
-// reset all global variables
-const initGame = (level) => {
-    currentLevel = level;
-    num1 = 0;
-    num2 = 0;
+const init = (level) => {
     score = 0;
-    refresh();
+    sec = 59;
+    min = 1;
+    currentLevel = level;
+    newGame();
 }
 
-// Randomlly pinked numbers and operations
-const refresh = () => {
+// Init the game and set all variables to a new game
+const newGame = () => {
+    isGameOver = false;
     let questionId = document.querySelector("#questionId");
     let inputId = document.querySelector("#inputId");
     if (questionId && inputId) {
@@ -86,7 +89,7 @@ const checkAnswer = () => {
 
 // update the scoreDiv by the current score
 const updateScore = (num) => {
-    let scoreDiv = document.querySelector(".score");
+    let scoreDiv = document.querySelector("#scoreGame");
     if (scoreDiv) {
         score += num;
         if (score < 0) {
@@ -117,21 +120,93 @@ const updateScore = (num) => {
     }
 }
 
+const updateTime = (num) => {
+    let time = document.querySelector(".timer");
+    if (sec + num > 60) {
+        sec = (sec + num) - 60;
+        min++;
+    }
+    else if (sec + num < 0) {
+        sec = 60 + sec + num;
+        min--;
+    } else {
+        sec += num;
+    }
+    if (num > 0) {
+        time.style.color = "green";
+        time.style.scale = "1.1";
+        setTimeout(() => {
+            time.style.color = "black";
+            time.style.scale = "1";
+        }, 300)
+    } else {
+        time.style.color = "red";
+        time.style.scale = "1.1";
+        setTimeout(() => {
+            time.style.color = "black";
+            time.style.scale = "1";
+        }, 300)
+    }
+}
+
+// return random number 
 const getRandomNums = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
+// return random oparator
 const getRandomOperations = () => {
     let arr = ["+", "-", "x", ":"];
     return arr[getRandomNums(0, arr.length - 1)];
 }
 
+// timer count down
+const timerFunc = () => {
+    let time = document.querySelector(".timer");
+    setInterval(() => {
+        if (!isGameOver) {
+            if (time) {
+                if (sec < 0) {
+                    sec = 59;
+                    min--;
+                }
+                if (min < 0) {
+                    gameOver();
+                    return;
+                }
+                if (sec < 10) {
+                    time.innerHTML = `Time: ${min}:0${sec}`;
+                } else {
+                    time.innerHTML = `Time: ${min}:${sec}`;
+                }
+                sec--;
+            }
+        }
+
+    }, 1000)
+}
+
+// When the time is over switch to finish container
+const gameOver = () => {
+    let finishDiv = document.querySelector(".container-finish");
+    let gameDiv = document.querySelector(".container-game");
+    isGameOver = true;
+    if (finishDiv && gameDiv) {
+        gameDiv.style.display = "none";
+        finishDiv.style.display = "block";
+        let scoreFinish = document.querySelector("#scoreFinish");
+        scoreFinish.innerHTML = `Score: ${score}`;
+    }
+}
+
 window.addEventListener("load", () => {
+    timerFunc();
     let difficultyArr = document.querySelectorAll(".difficulty > div");
     let refreshId = document.querySelector("#refreshId");
     let sendBtn = document.querySelector("#sendBtn");
+    let finishBtn = document.querySelector(".finish-button");
     if (difficultyArr) {
         for (let level of difficultyArr) {
             level.addEventListener("click", () => {
@@ -140,16 +215,30 @@ window.addEventListener("load", () => {
         }
     }
     if (refreshId) {
-        refreshId.addEventListener("click", refresh);
+        refreshId.addEventListener("click", () => {
+            newGame();
+        });
     }
     if (sendBtn) {
         sendBtn.addEventListener("click", () => {
             if (checkAnswer()) {
                 updateScore(10);
-                refresh();
+                updateTime(5);
+                newGame();
             } else {
                 updateScore(-5);
+                updateTime(-10);
             }
         });
+    }
+    if (finishBtn) {
+        finishBtn.addEventListener("click", () => {
+            let finishDiv = document.querySelector(".container-finish");
+            let startDiv = document.querySelector(".container-start");
+            if (finishDiv && startDiv) {
+                finishDiv.style.display = "none";
+                startDiv.style.display = "block";
+            }
+        })
     }
 })
